26d35f6336b0e2c29e49e7fc043d7219cce5a19c
Iftekhar Ahmad <iftekhar.niet@gmail.com>
2024-02-09 19:28:55 +0530

    Invalid object check

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hallo there, I am from container.</h1>
+      <h1>Hallo there!!</h1>
       <MarketingApp />;
     </div>
   );
