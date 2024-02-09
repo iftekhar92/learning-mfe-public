79c8107847592c5fb5a94200a45c912920cb5f27
Iftekhar Ahmad <iftekhar.niet@gmail.com>
2024-02-09 17:01:43 +0530

    Update onboarding msg.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h2>Hi there! I am from container.</h2>
+      <h2>I am from container.</h2>
       <MarketingApp />;
     </div>
   );
