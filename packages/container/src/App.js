2aa5195ef25e9ae50f895aa0c02e740a9e3c6aa0
Iftekhar Ahmad <iftekhar.niet@gmail.com>
2024-02-09 16:44:54 +0530

    Update message

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h3>Hi there!</h3>
+      <h2>Hi there! I am from container.</h2>
       <MarketingApp />;
     </div>
   );
